beb396abb68f213fd28ad8726febad5444b5053a
Arghaya Mondal (Arghaya) <arghayamondal123@gmail.com>
2022-12-06 20:03:26 +0530

    Revert "initialize s3 bucket for capstone project"
    This reverts commit 99d3e10f8781b705034df3db3febef4a419f5622.

--- provider.tf
+++ provider.tf
@@ -1,12 +1,20 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.45.0"
+      source  = "hashicorp/aws"
+      version = "4.37.0"
+    }
+    curl = {
+      source  = "anschoewe/curl"
+      version = "1.0.2"
     }
   }
 }
 
 provider "aws" {
+  # Configuration options
+}
+
+provider "curl" {
   # Configuration options
 }
