3ca5afa76681e80dd82b99404ec692f8776e18c7
Arghaya Mondal (Arghaya) <arghayamondal123@gmail.com>
2022-12-06 19:54:00 +0530

    initialize s3 bucket for capstone project

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "arghaya-upgrad-backend-tf"
-    key    = "tf-state"
-    region = "us-east-1"
-  }
-}
--- provider.tf
+++ provider.tf
@@ -1,20 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "4.37.0"
-    }
-    curl = {
-      source  = "anschoewe/curl"
-      version = "1.0.2"
+      source = "hashicorp/aws"
+      version = "4.45.0"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
-}
-
-provider "curl" {
   # Configuration options
 }
